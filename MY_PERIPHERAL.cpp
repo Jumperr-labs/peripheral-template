5690d9d23b9d9c49bd897f4f976558eae72ecfe3
Jonathan Seroussi <seryoni@gmail.com>
2018-03-19 09:41:36 +0200

    Update MY_PERIPHERAL.cpp

--- MY_PERIPHERAL.cpp
+++ MY_PERIPHERAL.cpp
@@ -5,7 +5,7 @@ MY_PERIPHERAL::MY_PERIPHERAL() {
 //     SpiSlaveConfig spi_config = {
 //             .mosi_pin_number = GetPinNumber("si"), .miso_pin_number = GetPinNumber("so"),
 //             .ss_pin_number = GetPinNumber("cs"), .sclk_pin_number = GetPinNumber("sck"),
-//             .supported_spi_modes = SPI_MODE_0 | SPI_MODE_1 | SPI_MODE_2 | SPI_MODE_3,
+//             .supported_spi_modes = SPI_MODE_CPOL0_CPHA0 | SPI_MODE_CPOL0_CPHA1 | SPI_MODE_CPOL1_CPHA0 | SPI_MODE_CPOL1_CPHA1,
 //             .max_frequency = 5000000, .bit_order = MSB_FIRST
 //     };
 
