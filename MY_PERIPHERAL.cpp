cdcc3488df212d2e7b042cf9ec627525bd8b8818
Jonathan Seroussi <seryoni@gmail.com>
2018-03-19 09:35:07 +0200

    Update MY_PERIPHERAL.cpp

--- MY_PERIPHERAL.cpp
+++ MY_PERIPHERAL.cpp
@@ -1,7 +1,15 @@
 #include "MY_PERIPHERAL.h"
+#include <iostream>
 
 MY_PERIPHERAL::MY_PERIPHERAL() {
-    // TODO: Add contructor logic here. Get pin numbers, create SPI slave, etc.
+//     SpiSlaveConfig spi_config = {
+//             .mosi_pin_number = GetPinNumber("si"), .miso_pin_number = GetPinNumber("so"),
+//             .ss_pin_number = GetPinNumber("cs"), .sclk_pin_number = GetPinNumber("sck"),
+//             .supported_spi_modes = SPI_MODE_0 | SPI_MODE_1 | SPI_MODE_2 | SPI_MODE_3,
+//             .max_frequency = 5000000, .bit_order = MSB_FIRST
+//     };
+
+//     spi_slave_ = CreateSpiSlave(spi_config);    
 }
 
 void MY_PERIPHERAL::Main() {
