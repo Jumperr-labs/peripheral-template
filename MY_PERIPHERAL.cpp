bb063d88fb780bdc4a87c7a8f20dfbabe767f7f7
Dan Shemesh <dan@jumper.io>
2018-04-12 14:01:11 +0300

    comment in cpp

--- MY_PERIPHERAL.cpp
+++ MY_PERIPHERAL.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 
 MY_PERIPHERAL::MY_PERIPHERAL() {
-//     SpiSlaveConfig spi_config = {
-//             .mosi_pin_number = GetPinNumber("si"), .miso_pin_number = GetPinNumber("so"),
-//             .ss_pin_number = GetPinNumber("cs"), .sclk_pin_number = GetPinNumber("sck"),
-//             .supported_spi_modes = SPI_MODE_CPOL0_CPHA0 | SPI_MODE_CPOL0_CPHA1 | SPI_MODE_CPOL1_CPHA0 | SPI_MODE_CPOL1_CPHA1,
-//             .max_frequency = 5000000, .bit_order = MSB_FIRST
-//     };
+    /************************************************************************************
+    TODO: Modify the following parameters for according to the peripheral's spec.
+    ************************************************************************************/
+    SpiSlaveConfig spi_config = {
+            .mosi_pin_number = GetPinNumber("si"), .miso_pin_number = GetPinNumber("so"),
+            .ss_pin_number = GetPinNumber("cs"), .sclk_pin_number = GetPinNumber("sck"),
+            .supported_spi_modes = SPI_MODE_CPOL0_CPHA0 | SPI_MODE_CPOL0_CPHA1 | SPI_MODE_CPOL1_CPHA0 | SPI_MODE_CPOL1_CPHA1,
+            .max_frequency = 5000000, .bit_order = MSB_FIRST
+    };
 
-//     spi_slave_ = CreateSpiSlave(spi_config);    
+    spi_slave_ = CreateSpiSlave(spi_config);    
 }
 
 void MY_PERIPHERAL::Main() {
