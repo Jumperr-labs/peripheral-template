5cc615d0e392e3eda996a98b2d0c1bedfb888aef
inbarmoskovich <32809763+inbarmoskovich@users.noreply.github.com>
2018-04-17 18:19:17 +0300

    rename script

---
+++ create_peripheral.py
@@ -0,0 +1,34 @@
+import re
+import os
+import subprocess
+
+string_to_replace = "MY_PERIPHERAL"
+peripheral_name = "MY_PERIPHERAL"
+temp_cpp_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'temp_peripheral', 'MY_PERIPHERAL.cpp'))
+temp_h_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'temp_peripheral', 'MY_PERIPHERAL.h'))
+temp_makefile = os.path.abspath(os.path.join(os.path.dirname(__file__), 'temp_peripheral', 'Makefile'))
+
+print "Enter your peripheral name (press enter in order to use the default name '{}'):".format(peripheral_name),
+peripheral_name = raw_input()
+
+cpp_file = os.path.abspath(os.path.join(os.path.dirname(__file__),  peripheral_name + ".cpp"))
+h_file = os.path.abspath(os.path.join(os.path.dirname(__file__),  peripheral_name + ".h"))
+makefile = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Makefile'))
+
+with open(temp_cpp_file, 'r') as fileinput, open(cpp_file, 'w+') as fileoutput:
+    for line in fileinput:
+        line = re.sub(string_to_replace, peripheral_name, line.rstrip()) + "\n"
+        fileoutput.write(line)
+
+with open(temp_h_file, 'r') as fileinput, open(h_file, 'w+') as fileoutput:
+    for line in fileinput:
+        line = re.sub(string_to_replace, peripheral_name, line.rstrip()) + "\n"
+        fileoutput.write(line)
+
+with open(temp_makefile, 'r') as fileinput, open(makefile, 'w+') as fileoutput:
+    for line in fileinput:
+        line = re.sub(string_to_replace, peripheral_name, line.rstrip()) + "\n"
+        fileoutput.write(line)
+
+
+subprocess.check_call('make')
