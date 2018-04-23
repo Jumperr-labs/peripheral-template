4ee02125f4dcaa93d1fa2d580f2790eebe30d89a
inbarmoskovich <32809763+inbarmoskovich@users.noreply.github.com>
2018-04-23 14:28:53 +0300

    communication-type

--- temp_peripheral/general/MY_PERIPHERAL.cpp
+++ temp_peripheral/general/MY_PERIPHERAL.cpp
@@ -2,17 +2,6 @@
 #include <iostream>
 
 MY_PERIPHERAL::MY_PERIPHERAL() {
-    /************************************************************************************
-    TODO: Modify the following parameters for according to the peripheral's spec.
-    ************************************************************************************/
-    SpiSlaveConfig spi_config = {
-            .mosi_pin_number = GetPinNumber("si"), .miso_pin_number = GetPinNumber("so"),
-            .ss_pin_number = GetPinNumber("cs"), .sclk_pin_number = GetPinNumber("sck"),
-            .supported_spi_modes = SPI_MODE_CPOL0_CPHA0 | SPI_MODE_CPOL0_CPHA1 | SPI_MODE_CPOL1_CPHA0 | SPI_MODE_CPOL1_CPHA1,
-            .max_frequency = 5000000, .bit_order = MSB_FIRST
-    };
-
-    spi_slave_ = CreateSpiSlave(spi_config);    
 }
 
 void MY_PERIPHERAL::Main() {
